41f9ee82512d8a78cd16625955f33e13faf2d944
yeying075 <3504859149@qq.com>
2023-06-19 21:17:27 +0800

    first

---
+++ djangoZjj/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('zjj/', include('zjj.urls')),
+    # path('admin/', admin.site.urls),
+]
